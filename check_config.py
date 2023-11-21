3a8142c9ac0c88eea5536db25f43cc31940a77a0
Story DeWeese <sdeweese@alumni.scu.edu>
2023-11-21 14:03:25 -0800

    Update check_config.py

--- check_config.py
+++ check_config.py
@@ -7,11 +7,12 @@ check_features["VRF"] = ["ip vrf", "vrf definition"]
 
 check_features["VRF_INTERFACE"] = ["vrf forwarding"]
 
-# input config from the customer - change this to a text file
-customer_input = "router bgp mdt default switching tme team rocks ! interface GigabitEthernet0/0 vrf forwarding Mgmt-vrf no ip address shutdown negotiation auto !"
+#print(check_features)
 
-print("Hello, AMA!")
-print(check_features)
+# input config from the customer - change this to a text file
+customer_input = "router bgp mdt default switching tme team rocks ! interface TenGigabitEthernet1/0/1 vrf forwarding Mgmt-vrf no ip address shutdown negotiation auto ! interface TenGigabitEthernet1/0/2  ! interface TenGigabitEthernet1/0/3 "
+
+#print(check_features)
 
 features_in_customer_input = {}
 features_in_customer_input["MULTICAST"] = []
@@ -23,8 +24,8 @@ for feature in check_features["MULTICAST"]:
         features_in_customer_input["MULTICAST"].append(feature)
         
 
-print("Features in customer_input['Multicast']")
-print(features_in_customer_input)
+#print("Features in customer_input['Multicast']")
+#print(features_in_customer_input)
 
 
 
@@ -35,17 +36,21 @@ print(features_in_customer_input)
 # look for the feature
 for feature in check_features["VRF_INTERFACE"]:
     
-    print(feature)
+    #print(feature)
     
-    ## end at !
-    x = customer_input.split("! interface GigabitEthernet0/0")
-    print(x)
-    y = x[1].split("!") # will it always be the 1st item in list?
-    print(y)
-    print(y[0])
-    split_feature = y[0]
-    if(split_feature in customer_input):
-       features_in_customer_input["VRF_INTERFACE"].append(feature)
+    
+    # iterate through a loop 
+    # define start and end point for one interface
+    interface = customer_input.split("! interface") # everything after the first occourance of ' interface" for example: TenGigabitEthernet1/0/1 Mgmt-vrf no ip address shutdown negotiation auto ! interface TenGigabitEthernet1/0/2 vrf forwarding ! interface TenGigabitEthernet1/0/3 
+
+    #end = start[1].split("!") # everything between the start and the next '!' for example:  [ 'TenGigabitEthernet1/0/1 Mgmt-vrf no ip address shutdown negotiation auto', ' interface TenGigabitEthernet1/0/2 vrf forwarding ! interface TenGigabitEthernet1/0/3'] 
+
+    for i in interface:
+    	if i != 0: # don't check stuff in the 0th place of the list because it's the config before the first interface for example: router bgp mdt default 
+        #split_feature = end[0] #  TenGigabitEthernet1/0/1 Mgmt-vrf no ip address shutdown negotiation auto 
+            if feature in i: # only check the substring of customer_input
+               features_in_customer_input["VRF_INTERFACE"].append(feature)
+
         
 print("Features in customer_input")
 print(features_in_customer_input)
