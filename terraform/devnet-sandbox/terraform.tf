26ca6f8cb61e7688dd20b08e1ee7e02f9f0136c9
Story DeWeese <sdeweese@alumni.scu.edu>
2024-06-12 13:07:18 -0700

    Add sandbox examples

---
+++ terraform/devnet-sandbox/terraform.tf
@@ -0,0 +1,29 @@
+# initialize terraform using 'terraform init'
+# plan out what will happen using 'terraform plan'
+# apply this terraform file using 'terraform apply -auto-approve'
+# remove this config using 'terraform destroy' & confirm removing the config using 'yes'
+
+# This will create Loopback800 the DevNet Sandbox
+
+terraform {
+  required_providers {
+    iosxe = {
+      source = "CiscoDevNet/iosxe"
+      version = "0.5.5"
+    }
+  }
+}
+
+provider "iosxe" {
+  url = "https://devnetsandboxiosxe.cisco.com"
+  insecure = true
+  username = "admin"
+  password = "C1sco12345"
+}
+
+resource "iosxe_cli" "example" {
+  cli = <<-EOT
+  interface Loopback880
+  description configured-via-restconf-cli
+  EOT
+}
